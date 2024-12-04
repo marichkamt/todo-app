3883cd1dee84de1b16f6fde0f32719f4b891ea9f
marichkamt <emilycry1999@gmail.com>
2024-12-04 14:28:56 +0100

    clone todo-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
-import './App.scss';
-
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import { UserWarning } from './UserWarning';
+import { USER_ID } from './api/todos';
+import { TodoApp } from './components/TodoApp';
 
 export const App: React.FC = () => {
-  return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
-    </div>
-  );
+  if (!USER_ID) {
+    return <UserWarning />;
+  }
+
+  return <TodoApp />;
 };
